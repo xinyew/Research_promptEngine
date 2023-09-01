9f87ddc4cd4333318a45e2313472290ca69aa12a
promptEngine <wangxinye1996@gmail.com>
2023-09-01 19:23:25 +0000

    wsgi is so stupid

--- webapps/settings.py
+++ webapps/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 p = os.path.join(os.getcwd(), 'secret_django')
+p = '/home/ubuntu/Research_promptEngine/secret_django'
 secret = ''
 with open(p, 'r') as f:
     secret += f.read()[:-1]
